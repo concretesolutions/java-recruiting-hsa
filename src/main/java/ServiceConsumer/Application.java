65fb8c3772a486450cd94e9ec07f69c641ef3631
Dreamseeker5 <edgardhernandezp@gmail.com>
2019-06-01 01:53:24 -0400

    Added funcionality to print out the json fetched from the service category API

--- src/main/java/ServiceConsumer/Application.java
+++ src/main/java/ServiceConsumer/Application.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,6 +14,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.StringWriter;
+
+
 
 public class Application {
 
@@ -21,6 +28,18 @@ public class Application {
         RestTemplate restTemplate = new RestTemplate();
         Coupon[] coupons = restTemplate.getForObject("https://cs-hsa-api-coupons.herokuapp.com/coupons", Coupon[].class);
         Category category = restTemplate.getForObject("https://cs-hsa-api-categories.herokuapp.com/categories", Category.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        try {
+            StringWriter stringEmp = new StringWriter();
+            objectMapper.writeValue(stringEmp, category);
+            System.out.println(stringEmp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         try {
             Date date = new Date();
