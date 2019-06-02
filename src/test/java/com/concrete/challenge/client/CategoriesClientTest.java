e94ae4118977d69d0444f8f14178e5606f7857e8
felipegmch <felipegmch@gmail.com>
2019-06-02 16:09:48 -0400

    Added clients tests for categories and clients

---
+++ src/test/java/com/concrete/challenge/client/CategoriesClientTest.java
@@ -0,0 +1,14 @@
+package com.concrete.challenge.client;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CategoriesClientTest {
+
+    @InjectMocks
+    private CategoriesClient categoriesClient;
+
+
+}
---
+++ src/test/java/com/concrete/challenge/client/CouponsClientTest.java
@@ -0,0 +1,12 @@
+package com.concrete.challenge.client;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CouponsClientTest {
+
+    @InjectMocks
+    private CouponsClient couponsClient;
+}
