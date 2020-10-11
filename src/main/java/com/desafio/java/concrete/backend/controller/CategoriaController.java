e48b34d6d1d9eabadf8289a975f4409b9d2d8548
Veronica Escobar <veronica.escobarar@gmail.com>
2020-10-11 18:52:23 -0300

    fix Dockerfile

--- src/main/java/com/desafio/java/concrete/backend/controller/CategoriaController.java
+++ src/main/java/com/desafio/java/concrete/backend/controller/CategoriaController.java
@@ -6,11 +6,13 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "Categories")
+@RequestMapping(value="/category")
 public class CategoriaController {
 
     @Autowired
