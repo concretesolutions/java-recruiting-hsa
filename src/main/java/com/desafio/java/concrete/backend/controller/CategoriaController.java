413a740074d49a831add37a4342806e93f6e213e
Veronica Escobar <veronica.escobarar@gmail.com>
2020-10-11 21:54:40 -0300

    fix

--- src/main/java/com/desafio/java/concrete/backend/controller/CategoriaController.java
+++ src/main/java/com/desafio/java/concrete/backend/controller/CategoriaController.java
@@ -5,30 +5,25 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Api(tags = "Categories")
-@RequestMapping(value="/category")
+@RestController
 public class CategoriaController {
 
     @Autowired
     CategoriesServices categoriesServices;
 
     @ApiOperation(value = "Top Categorias", notes = "Retorna el Top 5 en categorias.")
-    @GetMapping(value = "/top-categories", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public @ResponseBody
-    String getTopCategories() {
+    @RequestMapping(value = "/top-categories", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String getTopCategories() {
         return categoriesServices.getTopCategories();
     }
 
     @ApiOperation(value = "Categorias fueras del top", notes = "Retorna las categorias que no estan dentro del top 5")
-    @GetMapping(value = "/getCategories", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public @ResponseBody
-    String getCategories() {
+    @RequestMapping(value = "/getCategories", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String getCategories() {
         return categoriesServices.getCategories();
     }
 
