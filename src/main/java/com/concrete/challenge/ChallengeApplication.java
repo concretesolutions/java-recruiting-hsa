663ff3492173a3513b7df777778a263e933b0bb4
carlos apablaza <c.apablaza.z@gmail.com>
2020-11-02 15:26:58 -0300

    add swagger support for api

--- src/main/java/com/concrete/challenge/ChallengeApplication.java
+++ src/main/java/com/concrete/challenge/ChallengeApplication.java
@@ -6,9 +6,15 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
@@ -19,4 +25,11 @@ public class ChallengeApplication {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.concrete.challenge")).build();
+	}
+
 }
