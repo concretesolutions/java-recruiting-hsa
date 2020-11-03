f999d260d1312c1a89e941877df9807ff81a1e90
Matias Gomez <matias.gomeza@usach.cl>
2020-11-03 20:32:38 -0300

    Base proyecto. Springboot, lombok, jpa, springweb, java8.

---
+++ src/main/java/com/accenture/test/matias/JavatestApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.test.matias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/test/matias/JavatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.test.matias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
