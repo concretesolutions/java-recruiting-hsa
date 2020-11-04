7035d3df3e9050cebd0711814e001f67965cbd17
Matias Gomez <matias.gomeza@usach.cl>
2020-11-03 21:01:03 -0300

    Se quita jpa y se corrige inicializacion de la app

--- src/main/java/com/accenture/test/matias/JavatestApplication.java
+++ src/main/java/com/accenture/test/matias/JavatestApplication.java
@@ -2,8 +2,9 @@ package com.accenture.test.matias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JavatestApplication {
 
 	public static void main(String[] args) {
