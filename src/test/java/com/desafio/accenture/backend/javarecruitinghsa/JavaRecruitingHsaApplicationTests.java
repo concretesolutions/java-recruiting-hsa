4ac6459b43b08ae37090efd0963b3d03682ac1ed
Alvaro Montilla <amsystemdevelopment@gmail.com>
2020-11-30 22:43:32 -0300

    initial project

---
+++ src/test/java/com/desafio/accenture/backend/javarecruitinghsa/JavaRecruitingHsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.accenture.backend.javarecruitinghsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRecruitingHsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
