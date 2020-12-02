80e4a207ab2f943ffc096547302a64bb95b3e35e
Felipe <felipe@talavera.io>
2020-12-01 22:07:38 -0300

    more config

--- src/main/java/cl/talavera/api/config/Config.java
+++ src/main/java/cl/talavera/api/config/Config.java
@@ -1,2 +1,31 @@
-package cl.talavera.api.config;public class Config {
+package cl.talavera.api.config;
+
+import cl.talavera.api.core.interactor.TopCategoriesInteractor;
+import cl.talavera.api.core.port.CategoriesPresenterPort;
+import cl.talavera.api.core.port.CategoryDaoPort;
+import cl.talavera.api.core.port.TopCategoriesInteractorPort;
+import cl.talavera.api.data.CategoryDao;
+import cl.talavera.api.data.CategoryRestRepository;
+import cl.talavera.api.rest.CategoriesPresenter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public TopCategoriesInteractorPort interactor() {
+
+        return new TopCategoriesInteractor(dao(),presenter());
+    }
+    @Bean
+    protected CategoryDaoPort dao() {
+
+        CategoryRestRepository repo = new CategoryRestRepository();
+        return new CategoryDao(repo);
+    }
+
+    @Bean
+    protected CategoriesPresenterPort presenter() {
+        return new CategoriesPresenter();
+    }
 }
